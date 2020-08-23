b736b4921a1e5a6f9296906184700914d91e44b8
Christopher Falcon <chrisfalcon59@hotmail.com>
2020-08-23 17:46:09 -0400

    Add Nav bar and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { AppContextProvider } from './context/AppContext';
 import ContextDemo from './components/ContextDemo';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import './App.css';
-
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import NavBar from './components/NavBar';
 const App = () => {
   const [serverMessage, setServerMessage] = useState('');
 
@@ -17,11 +20,14 @@ const App = () => {
 
   return (
     <AppContextProvider>
-      <div id="demo">
-        <h3>Hello from client/src/App.js</h3>
-        <ContextDemo />
-        <h3>{serverMessage}</h3>
-      </div>
+      <NavBar />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+      </BrowserRouter>
     </AppContextProvider>
   );
 };

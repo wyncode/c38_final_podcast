0d65dab5ca44c0093aed0f42c8cb5945cab877bc
Leo Policastro <43428385+leopolicastro@users.noreply.github.com>
2020-08-28 09:01:13 -0400

    Update podcast.router.js

--- server/features/podcast/podcast.router.js
+++ server/features/podcast/podcast.router.js
@@ -8,17 +8,13 @@ const {
     login,
     addPodCastToCategory
 } = require("./podcast.controller") ;
-// router.route("/getAllCategories").get(getAllCategories)
 router.get("/getAllCategories", getAllCategories)
 
 
-// router.route("/addCategory").post(AddCategories)
 router.post("/addCategory", AddCategories)
 
-// router.route("/getAllPodCast").get(getAllPodCast)
 router.get("/getAllPodCast", getAllPodCast)
 
-// router.route("/getAllPodCastOfSingleCategory").get(getAllPodCastOfSingleCategory)
 router.route("/getAllPodCastOfSingleCategory").post(getAllPodCastOfSingleCategory)
 // login api 
 router.route( '/login' ).post( login )

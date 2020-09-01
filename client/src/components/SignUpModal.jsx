b8c6408939decb013eb4980042b39111b5f0e0cf
J Nicolas Perez <67791567+jnicolasperez@users.noreply.github.com>
2020-09-01 15:22:59 -0400

    Update SignUpModal.jsx

--- client/src/components/SignUpModal.jsx
+++ client/src/components/SignUpModal.jsx
@@ -1,15 +1,30 @@
 import React, { useState, useContext } from 'react';
-import { Modal, Form, Button, Nav } from 'react-bootstrap';
+import { Modal, Form, Button } from 'react-bootstrap';
 import { AppContext } from '../context/AppContext';
 import axios from 'axios';
 
 const SignUpModal = ({ history }) => {
   const { signUpModalOpen, setSignUpModalOpen } = useContext(AppContext);
+  const { setCurrentUser } = useContext(AppContext);
   const [formData, setFormData] = useState(null);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleModal = (event) => {
     event.preventDefault();
     setSignUpModalOpen(!signUpModalOpen);
+  };
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/users', formData);
+      sessionStorage.setItem('user', response.data);
+      setCurrentUser(response.data.user);
+      history.push('/');
+    } catch (error) {
+      console.log('SignUp Error: ', error);
+    }
   };
 
   return (
@@ -21,16 +36,29 @@ const SignUpModal = ({ history }) => {
       show={signUpModalOpen}
       backdrop="static"
     >
-      <Form className="container d-flex flex-column align-items-center justify-content-center fullscreen">
+      <Form
+        className="container d-flex flex-column align-items-center justify-content-center fullscreen"
+        onSubmit={handleSignUp}
+      >
         <Form.Text>Sign Up | WynCast</Form.Text>
         <Form.Group>
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Email" name="email" />
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Username" name="username" />
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group>
@@ -39,6 +67,7 @@ const SignUpModal = ({ history }) => {
             typep="password"
             placeholder="Password"
             name="password"
+            onChange={handleChange}
           />
         </Form.Group>
         <Button

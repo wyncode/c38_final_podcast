e2ac95dbef1bb6680f6e18d958c5150b3507059c
J Nicolas Perez <67791567+jnicolasperez@users.noreply.github.com>
2020-09-01 15:20:53 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { AppContextProvider } from './context/AppContext';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 import AboutYou from './pages/AboutYou';
+import Results from './pages/Results';
 import LoginModal from './components/LoginModal';
 import SignUpModal from './components/SignUpModal';
+import PrivateRoute from './components/PrivateRoute';
+import LibraryMain from './components/LibraryMain';
 
 const App = () => {
   return (
@@ -13,7 +15,11 @@ const App = () => {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
           <Route exact path="/About-Yourself" component={AboutYou} />
+          <Route exact path="/Results" component={Results} />
+          <PrivateRoute exact path="/library-main" component={LibraryMain} />
         </Switch>
         <LoginModal />
         <SignUpModal />
@@ -23,3 +29,4 @@ const App = () => {
 };
 
 export default App;
+
